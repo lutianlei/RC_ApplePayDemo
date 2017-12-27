bbde1d8926a7c4401d79755d1b95570ffc2724c0
lutianlei <lutianlei@haodai.net>
2017-12-27 16:20:03 +0800

    Initial Commit

---
+++ RayApplePayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RayApplePayDemo
+//
+//  Created by lutianlei on 2017/12/27.
+//  Copyright © 2017年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RayApplePayDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RayApplePayDemo
+//
+//  Created by lutianlei on 2017/12/27.
+//  Copyright © 2017年 Ray. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
